dafe7b79ce6bba87c39d0b0f19310ebd1f8e8b48
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2024-10-03 13:11:47 +0530

    done place orderPage  and frontend

--- src/pages/PalceOrder/PalceOrder.jsx
+++ src/pages/PalceOrder/PalceOrder.jsx
@@ -1,10 +1,53 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-
+import React, { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 import "./PlaceOrder.css";
 
 const PalceOrder = () => {
-  return <div></div>;
+  const { getTotalCartAmount } = useContext(StoreContext);
+  return (
+    <from className="place-order">
+      <div className="place-order-left">
+        <p className="title"> Delivery Information </p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First name" />
+          <input type="text" placeholder="last name" />
+        </div>
+        <input type="email" placeholder="Email address" />
+        <input type="text=" placeholder="street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Phone" />
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+          <div className="cart-total-deetails">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-deetails">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-deetails">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+          </div>
+          <button >PROCEED TO PAYMENT</button>
+        </div>
+        
+      </div>
+    </from>
+  );
 };
 
 export default PalceOrder;
